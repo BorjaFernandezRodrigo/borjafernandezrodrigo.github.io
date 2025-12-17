5eb9f84946004d3a5dea46c6bd5fa604e6afc344
Borja Fernandez Rodrigo <borja.fernandez.rodrigo@gmail.com>
2025-12-17 19:18:08 +0100

    config change mode tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,19 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: 'var(--color-primary)',
+        secondary: 'var(--color-secondary)',
+        'background-light': 'var(--color-background-light)',
+        'background-dark': 'var(--color-background-dark)',
+        'surface-dark': 'var(--color-surface-dark)',
+      },
+      backgroundColor: {
+        'background-light': 'var(--color-background-light)',
+        'background-dark': 'var(--color-background-dark)',
+      },
+    },
   },
   plugins: [],
 }
