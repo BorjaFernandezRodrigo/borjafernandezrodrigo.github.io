835acee130b0717397c48f282fa55ca581b692cb
Borja Fernandez Rodrigo <borja.fernandez.rodrigo@gmail.com>
2025-12-17 19:18:33 +0100

    change: inject theme service

--- src/js/Layout.js
+++ src/js/Layout.js
@@ -1,15 +1,15 @@
 import { config } from '../config/config'
 import { Header } from './components/Header.js'
-import { Navbar } from './components/navbar/navbar.js'
+import { Navbar } from './components/navbar/Navbar.js'
 import { Projects } from './components/Projects'
 import { Stats } from './components/Stats'
 import { TechStack } from './components/TechStack'
 
 export class Layout {
-  constructor(i18n) {
+  constructor(i18n, theme) {
     this.i18n = i18n
     this.header = new Header(this.i18n)
-    this.navbar = new Navbar(this.i18n, config.menuItems)
+    this.navbar = new Navbar(this.i18n, theme, config.menuItems)
     this.stats = new Stats({}, this.i18n)
     this.techStack = new TechStack(this.i18n)
     this.projects = new Projects(this.i18n)
@@ -26,7 +26,6 @@ export class Layout {
       <main class="z-10 flex-1 h-full overflow-y-auto overflow-x-hidden order-1 md:order-2 scroll-smooth">
             <div class="max-w-[1200px] mx-auto p-4 md:p-8 lg:p-12 flex flex-col gap-24">
                 <section id="header">${this.header.render()}</section>
-                
             </div>
       </main>
     </div>`
--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import './css/style.css'
 import { config } from './config/config.js'
-import { I18n } from './js/i18n.js'
+import { I18n } from './js/services/I18n.js'
+import { Theme } from './js/services/Theme.js'
 import { Layout } from './js/layout.js'
 
 const i18n = new I18n(config.defaultLanguage, config.supportedLanguages)
 await i18n.loadLanguage(config.defaultLanguage)
 
-const layout = new Layout(i18n)
+const theme = new Theme()
+theme.loadTheme()
+
+const layout = new Layout(i18n, theme)
 
 document.getElementById('app').innerHTML = layout.render()
 
